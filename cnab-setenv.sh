7b2742530f151079b27d31f6604f214ff0a1f37c
nunix <nnd@ferring.com>
2019-03-12 07:00:55 +0100

    modified porter.yaml url from private to public repo

--- cnab-setenv.sh
+++ cnab-setenv.sh
@@ -5,10 +5,10 @@ export DISTRO_IMAGE="nunix/wslbkhat"
 export DISTRO_NAME=wslbkhat
 export DISTRO_PATH=/github/${DISTRO_NAME}
 export DISTRO_SOURCE=/github/${DISTRO_NAME}.tar
-export PORTER_DEP=/github/cnabwslbuilder/porter.yaml
+export DOCKERFILE_SOURCE="https://raw.githubusercontent.com/nunix/wslbkhat/master/porter.yaml"
 export DOCKER_SOURCE=docker
 export DOCKER_APP_SOURCE="/usr/bin/docker-app"
 export WSL_SOURCE=wsl.exe
 
 ### CALL PORTER ###
-porter run --action $@ --file https://raw.githubusercontent.com/nunix/wslbkhat/master/porter.yaml?token=AA3Sko3-sBy84SUl_clYPr0JaF7dl_EOks5chl08wA%3D%3D --debug
+porter run --action $@ --file $DOCKERFILE_SOURCE --debug
