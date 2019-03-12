16dc7031e50819e914c7990415f665faea592240
nunix <nnd@ferring.com>
2019-03-12 07:10:52 +0100

    corrected values and added dotfiles to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
 FROM ubuntu:19.04
 
-RUN apt update && apt upgrade -y
-RUN apt install -y vim wget curl sudo
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install -y apt-utils wget curl sudo git
 
-RUN useradd -m bk
+RUN useradd -m -s /usr/bin/bash bk
 RUN echo "bk    ALL=(ALL:ALL)   NOPASSWD: ALL" >> /etc/sudoers
+
+RUN git clone https://github.com/bketelsen/dotfiles.git /home/bk/dotfiles
+RUN mv /home/bk/dotfiles/* /home/bk/
+RUN /bin/sh /home/bk/bin/1*.sh
+RUN /bin/sh /home/bk/bin/2*.sh
+RUN /bin/sh /home/bk/bin/3*Linux
 
 USER bk
 WORKDIR /home/bk
--- cnab-setenv.sh
+++ cnab-setenv.sh
@@ -5,10 +5,11 @@ export DISTRO_IMAGE="nunix/wslbkhat"
 export DISTRO_NAME=wslbkhat
 export DISTRO_PATH=/github/${DISTRO_NAME}
 export DISTRO_SOURCE=/github/${DISTRO_NAME}.tar
-export DOCKERFILE_SOURCE="https://raw.githubusercontent.com/nunix/wslbkhat/master/porter.yaml"
+export PORTERFILE_SOURCE="https://raw.githubusercontent.com/nunix/wslbkhat/master/porter.yaml"
+export DOCKERFILE_SOURCE="git@github.com:nunix/wslbkhat"
 export DOCKER_SOURCE=docker
 export DOCKER_APP_SOURCE="/usr/bin/docker-app"
 export WSL_SOURCE=wsl.exe
 
 ### CALL PORTER ###
-porter run --action $@ --file $DOCKERFILE_SOURCE --debug
+porter run --action $@ --file $PORTERFILE_SOURCE --debug
